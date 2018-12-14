a6a018b034ba82feb252aeea32fce3d5c4f822e9
crissmanz <cristina.manzoni@outlook.com>
2018-12-14 22:53:05 +0000

    added google maps api

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :riders
   resources :slogans
   resources :photos
+  resources :location
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_224441) do
+ActiveRecord::Schema.define(version: 2018_12_14_210021) do
+
+  create_table "locations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string "owner"
