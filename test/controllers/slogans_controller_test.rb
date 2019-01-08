e7ea9545866f91677bb9111819df354be37c86ec
crissmanz <cristina.manzoni@outlook.com>
2019-01-08 21:22:05 +0000

    BoulderBikeTour

--- test/controllers/slogans_controller_test.rb
+++ test/controllers/slogans_controller_test.rb
@@ -4,45 +4,4 @@ class SlogansControllerTest < ActionDispatch::IntegrationTest
   setup do
     @slogan = slogans(:one)
   end
-
-  test "should get index" do
-    get slogans_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_slogan_url
-    assert_response :success
-  end
-
-  test "should create slogan" do
-    assert_difference('Slogan.count') do
-      post slogans_url, params: { slogan: {  } }
-    end
-
-    assert_redirected_to slogan_url(Slogan.last)
-  end
-
-  # test "should show slogan" do
-  #   get slogan_url(@slogan)
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   get edit_slogan_url(@slogan)
-  #   assert_response :success
-  # end
-  #
-  # test "should update slogan" do
-  #   patch slogan_url(@slogan), params: { slogan: {  } }
-  #   assert_redirected_to slogan_url(@slogan)
-  # end
-
-  # test "should destroy slogan" do
-  #   assert_difference('Slogan.count', -1) do
-  #     delete slogan_url(@slogan)
-  #   end
-  #
-  #   assert_redirected_to slogans_url
-  # end
 end
--- test/models/slogan_test.rb
+++ test/models/slogan_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
 class SloganTest < ActiveSupport::TestCase
-  test "should not save slogan without first_name" do
-    slogan = Slogan.new
-    assert_not slogan.save, "Saved the slogan without a first_name"
-  end
 end
